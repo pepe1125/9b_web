67fe38ddff03a25d7f07a3a1d146a30705cacba6
VBenedek2006 <90269623+VBenedek2006@users.noreply.github.com>
2022-04-25 19:44:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var termek1 = 1000
 var termek2 = 2600
 var termek3 = 8900
 var termek4 = 7000
+var termek5 = 3990
+var termek6 = 7000
+var termek7 = 3500
+var termek8 = 9999
 
 function kosarba(termek){
     db=Number(prompt('Add meg mennyit szeretnél?'))
