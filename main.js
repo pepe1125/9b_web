36892510a819f2d01a08d3aa6449996bbaac6e1d
VBenedek2006 <90269623+VBenedek2006@users.noreply.github.com>
2022-04-27 21:21:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,26 @@ function fizetes(){
     alert('Köszönjük a megrendelésed!')
     open.window(index.html , self_)
 }
+function torles() {
+    localStorage.clear()
+}
+function kosar() {
+    var list = "<tr><th style=\"margin:auto\">Termék neve: </th><th>darabszáma: </th><th>Ára: </th></tr>\n"
+
+    var ossz = 0;
+    var darab = 0;
+    var key = "";
+
+
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+        key = localStorage.key(i);
+        darab = parseInt(localStorage.getItem(key));
+        list += "<tr><td>" + key + "</td><td>" + "<button style=\"width:100%\" onclick='ModifyItem(\"" + key + "\", " + "prompt(\"Mennyi legyen?\")" + ")'>" + darab + "</button>" + "</td>" + "</td><td>" + (window[key] * darab) + " Ft" + "</td><td>" + "<button style=\"width:100%; background-color:red\" onclick='RemoveItem(\"" + key + "\")'>X</button>" + "</td></tr>";
+        ossz += window[key] * darab;
+    }
+    list += "<tr> <th>VÉGÖSSZEG: </th> <th> </th> <th>" + ossz + " Ft" + "</th></tr>"
+
+
+
+    document.getElementById('tabla').innerHTML = list
+}
