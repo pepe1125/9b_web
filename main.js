a3ba53c8f9432f9ed707ec27a9f10f6a75d03dc2
VBenedek2006 <90269623+VBenedek2006@users.noreply.github.com>
2022-05-08 17:46:42 +0200

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function torles() {
     localStorage.clear()
 }
 function kosar() {
-    var list = "<tr><th style=\"margin:auto\">Termék neve: </th><th>darabszáma: </th><th>Ára: </th></tr>\n"
+   var list = "<tr><th style=\"margin:auto\">Termék neve: </th><th>darabszáma: </th><th>Ára: </th></tr>\n"
 
     var ossz = 0;
     var darab = 0;
@@ -39,9 +39,23 @@ function kosar() {
         list += "<tr><td>" + key + "</td><td>" + "<button style=\"width:100%\" onclick='ModifyItem(\"" + key + "\", " + "prompt(\"Mennyi legyen?\")" + ")'>" + darab + "</button>" + "</td>" + "</td><td>" + (window[key] * darab) + " Ft" + "</td><td>" + "<button style=\"width:100%; background-color:red\" onclick='RemoveItem(\"" + key + "\")'>X</button>" + "</td></tr>";
         ossz += window[key] * darab;
     }
+
     list += "<tr> <th>VÉGÖSSZEG: </th> <th> </th> <th>" + ossz + " Ft" + "</th></tr>"
 
 
 
     document.getElementById('tabla').innerHTML = list
+}
+
+function termekek() {
+    var dbszam = 0;
+
+    for (var i = 0; i <= localStorage.length - 1; i++) {
+        key = localStorage.key(i);
+        console.log(key)
+        dbszam += Number(localStorage.getItem(key))
+    }
+
+    document.getElementById("darabszam").innerHTML = dbszam
+
 }
